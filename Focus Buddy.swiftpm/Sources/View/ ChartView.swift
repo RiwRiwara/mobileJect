a4068c116e88f865c2df13ac5c1d571a3b1ec30f
RiwRiwara <awirut2629@gmail.com>
2024-05-13 03:38:30 +0700

    Remove unused files and update file names for consistency

--- Focus Buddy.swiftpm/Sources/View/ ChartView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct ChartView: View {
-    var body: some View {
-         Color(AppColor.Darker)
-        // Define the content of the home page
-        Text("Welcome to the Chart!")
-    }
-}
--- Focus Buddy.swiftpm/Sources/View/SettingsView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct SettingsView: View {
-    var body: some View {
-         Color(AppColor.Darker)
-        Text("Welcome to the setting!")
-    }
-}
