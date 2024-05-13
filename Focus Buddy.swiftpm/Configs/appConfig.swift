e64941cf8aac3a4f729e115bde23c298ee809396
RiwRiwara <awirut2629@gmail.com>
2024-05-13 19:00:17 +0700

    refactor: update base URL for API endpoints

--- Focus Buddy.swiftpm/Configs/appConfig.swift
+++ Focus Buddy.swiftpm/Configs/appConfig.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 enum Endpoint {
-    static let baseURL = "https://www.pisan30.online"
+    static let baseURL = "https://www.pisan30.online/mobile"
     
     enum Path {
-        static let testApi = "/test",
-        static let getItems = "/getItems",
+        static let testApi = "/test"
+        static let getItems = "/getItems"
     }
 }
 
