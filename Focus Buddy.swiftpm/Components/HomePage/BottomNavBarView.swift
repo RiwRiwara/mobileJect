b3115f87f9b8ab3f73a38d460634bca2d2209639
RiwRiwara <awirut2629@gmail.com>
2024-05-14 05:02:32 +0700

    refactor: Add lazy loading for images in BottomNavBarView and HomeScreen

--- Focus Buddy.swiftpm/Components/HomePage/BottomNavBarView.swift
+++ Focus Buddy.swiftpm/Components/HomePage/BottomNavBarView.swift
@@ -1,18 +1,18 @@
-import SwiftUI
-
 struct BottomNavBarView: View {
+    @State private var isCartActive: Bool = false // State to track cart navigation
+
     var body: some View {
         HStack {
             BottomNavBarItem(image: Image(systemName: "house.fill"), action: {})
-            
-        NavigationLink(destination: CartScreen()) {
-                BottomNavBarItem(image: Image(systemName: "cart.fill"), action: {})
-            }
+            BottomNavBarItem(image: Image(systemName: "cart.fill"), action: { self.isCartActive = true }) // Activate cart
         }
         .padding()
         .background(Color.white)
         .clipShape(Capsule())
         .padding(.horizontal)
         .shadow(color: Color.blue.opacity(0.15), radius: 8, x: 2, y: 6)
+        .fullScreenCover(isPresented: $isCartActive, content: {
+            CartScreen() 
+        })
     }
 }
