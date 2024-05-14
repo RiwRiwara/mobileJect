36166023da9afbd6891a8c9e6ed38871eebb2a3b
RiwRiwara <awirut2629@gmail.com>
2024-05-14 12:11:17 +0700

    back

--- Focus Buddy.swiftpm/Components/HomePage/SearchResultsView.swift
+++ Focus Buddy.swiftpm/Components/HomePage/SearchResultsView.swift
@@ -1,25 +1,11 @@
 import SwiftUI
-import Combine
 
 struct SearchResultsView: View {
     let search: String
 
     @StateObject private var searchItems = DataManager()
     @State private var isFetchingData = true
-    @State private var searchText = ""
-    private var debouncedSearchText = ""
-    private var searchCancellable: AnyCancellable?
-
-    init(search: String) {
-        self.search = search
-        // Debounce the search text
-        searchCancellable = $searchText
-            .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
-            .sink { _ in
-                self.fetchSearch()
-            }
-    }
-
+    
     var body: some View {
         VStack {
             Text("Search Results")
@@ -28,15 +14,25 @@ struct SearchResultsView: View {
                 .padding()
 
             if isFetchingData {
-                Spacer()
-                Text("Loading...")
-                    .padding()
-            } else {
+            Spacer()
+            Text("Loading...")
+                .padding()
+                .onAppear {
+                        fetchSearch()
+                    }
+                    .onReceive(searchItems.$fetchedItems) { _ in
+                        isFetchingData = false
+                    }
+                    .onChange(of: search) { _ in
+                        isFetchingData = true
+                        fetchSearch()
+                    }
+            }else {
                 ScrollView(.horizontal, showsIndicators: false) {
                     LazyHStack {
                         ForEach(searchItems.fetchedItems, id: \.self) { item in
                             ProductCardViewApi(size: 180, item: item)
-                                .padding(.trailing, 10)
+                                .padding(.trailing, 10) 
                         }
                     }
                     .padding(.horizontal)
@@ -44,21 +40,9 @@ struct SearchResultsView: View {
             }
         }
         .padding(.horizontal, 5)
-        .onReceive(searchItems.$fetchedItems) { _ in
-            isFetchingData = false
-        }
-        .onAppear {
-            searchText = search
-        }
-        .onChange(of: search) { newValue in
-            searchText = newValue
-        }
     }
 
     private func fetchSearch() {
-        guard debouncedSearchText != searchText else { return }
-        searchItems.fetchItems(name: searchText)
-        debouncedSearchText = searchText
-        isFetchingData = true
+        searchItems.fetchItems(name: search )
     }
 }
